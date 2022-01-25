48affa13390b581318320df6fd419feab0d13bef
Suresh Pawar <58379912+sureshpawar86@users.noreply.github.com>
2022-01-25 20:44:02 +0530

    Update providers.tf
    updated

--- providers.tf
+++ providers.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
-  #profile    = "suresh-terraform"
-  access_key = "var.access_key"
-  secret_key = "var.secret_key"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
-
