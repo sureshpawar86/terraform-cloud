2a7b9130e28920cb0b4306864258a900be1056ba
sureshpawar86 <sureshpawar86@gmail.com>
2022-01-25 20:36:07 +0530

    adding varibales file

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+   
+}
+variable "secret_key" {
+}
+variable "region" {
+}
+variable "availabilityZone" {
+}
+variable "instanceTenancy" {
+}
+variable "dnsSupport" {
+}
+variable "dnsHostNames" {
+}
+variable "vpcCIDRblock" {
+}
+
